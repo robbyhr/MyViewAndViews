4d9802f00582888bd8f25be6919f68b9727cc16d
robbyhr <robbyhermando@gmail.com>
2020-04-09 23:00:45 +0700

    aku ganteng

---
+++ app/src/main/java/robby/hermando/myviewandviews/MainActivity.java
@@ -0,0 +1,18 @@
+package robby.hermando.myviewandviews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setTitle("Jual Hape");
+        }
+    }
+}
